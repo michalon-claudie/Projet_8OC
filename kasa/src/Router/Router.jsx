fd158832d88e62f9a38c74e6529bc99be042398b
Claudie.Michalon <michalon.claudie@outlook.fr>
2023-11-08 15:23:53 +0100

    "router ok"

--- kasa/src/Router/Router.jsx
+++ kasa/src/Router/Router.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { BrowserRouter,Routes, Route} from 'react-router-dom';
 import Home from "../pages/home"
-import Banner from "../component/Banner"
-import Footer from "../component/Footer"
+import About from "../pages/about"
+import Error from "../pages/errorPage"
+
 
 function Router() {
     return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/Banner" element={<Banner/>} />
-        <Route path="/Footer" element={<Footer/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/error" element={<Error/>} />
       </Routes>
     </BrowserRouter>
     );

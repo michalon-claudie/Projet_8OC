783a57547822cbbb5ab37f31fcdc96bd7b89a058
Claudie.Michalon <michalon.claudie@outlook.fr>
2023-11-08 17:56:31 +0100

    "try to add styles on header"

--- kasa/src/component/Header.jsx
+++ kasa/src/component/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import "../style/header.scss"
 
 export default function Header()
 {
     return (
-        <div>
-            <h1>Bienvenue sur mon Header</h1>
+        <div className='header'>
+            <img/>
+            <ul>
+                <li>
+                    
+                </li>
+            </ul>
         </div>
     )
 }
--- kasa/src/pages/home.jsx
+++ kasa/src/pages/home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Header from '../component/Header'
+import Footer from '../component/Footer'
 
 export default function Home()
 {
     return (
         <div>
+            <Header/>
             <h1>Bienvenue sur la page d'acceuil</h1>
+            <Footer/>
         </div>
     )
 }

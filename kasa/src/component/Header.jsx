410c62448fa3db3ed85bea3ace207d400cf7cc22
Claudie.Michalon <michalon.claudie@outlook.fr>
2023-11-09 10:44:19 +0100

    "add styles on header"

--- kasa/src/component/Header.jsx
+++ kasa/src/component/Header.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import "../style/header.scss"
+import logo from '../assets/LOGO.png'
 
 export default function Header()
 {
     return (
         <div className='header'>
-            <img/>
-            <ul>
+            <img src={logo} alt='logo_kasa'/>
+            <ul className='nav_header'>
                 <li>
-                    
+                   Acceuil 
+                </li>
+                <li>
+                    A propos
                 </li>
             </ul>
         </div>

978ad3b2ab97210a76846f3fb44757c85cbf4fd4
Claudie.Michalon <michalon.claudie@outlook.fr>
2023-11-09 10:47:25 +0100

    "add header on pages"

--- kasa/src/pages/errorPage.jsx
+++ kasa/src/pages/errorPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Header from '../component/Header'
 
 export default function Error()
 {
     return (
         <div>
+            <Header/>
             <h1>Error 404</h1>
         </div>
     )

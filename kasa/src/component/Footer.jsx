423c6208d14f15f2327adb1c2e9afa0d5b751026
Claudie.Michalon <michalon.claudie@outlook.fr>
2023-11-14 15:04:40 +0100

    "add styles on footer"

--- kasa/src/component/Footer.jsx
+++ kasa/src/component/Footer.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import "../style/components/footer.scss"
+import logo from '../assets/LOGO_white.png'
 
 export default function Footer()
 {
     return (
-        <div>
-            <h1>Bienvenue sur mon Footer</h1>
+        <div className='Footer'>
+            <img src={logo} alt='logo_kasa'/>
+            <p>© 2020 Kasa. All rights reserved</p>
         </div>
     )
 }

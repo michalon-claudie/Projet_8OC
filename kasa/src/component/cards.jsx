e546f67c48d3b6363bd10aa5732c4a4f43486b71
Claudie.Michalon <michalon.claudie@outlook.fr>
2023-11-15 11:47:32 +0100

    "page error created and variables for color too"

---
+++ kasa/src/component/cards.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default Cards()
+{
+    <div className='cardsContainer'>
+
+    </div>
+}
--- kasa/src/pages/errorPage.jsx
+++ kasa/src/pages/errorPage.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Header from '../component/Header'
+import Footer from '../component/Footer'
+import '../style/pages/error.scss'
 
 export default function Error()
 {
     return (
         <div>
             <Header/>
-            <h1>Error 404</h1>
+            <div className='errorBody'>
+                <h1>404</h1>
+                <p>Oups! La page que vous demandez n'existe pas.</p>
+                <a href="/">Retournez sur la page d'accueil</a>
+            </div>
+            <Footer/>
         </div>
     )
 }

1010406c95fa55f257ef0047ac6cd026b949fa6e
Claudie.Michalon <michalon.claudie@outlook.fr>
2023-11-24 12:09:47 +0100

    "add styles on homepage and media querries"

--- kasa/src/component/Header.jsx
+++ kasa/src/component/Header.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import "../style/components/header.scss"
 import logo from '../assets/LOGO.png'
+import { Link, useLocation } from "react-router-dom"
 
 export default function Header()
 {
+    const location = useLocation();
     return (
         <div className='header'>
             <img src={logo} alt='logo_kasa'/>
             <ul className='nav_header'>
                 <li>
-                   Acceuil 
+                    <Link to={'/'} className='link'>
+                        Acceuil 
+                    </Link>
                 </li>
                 <li>
-                    A propos
+                    <Link to={"/about"} className='link'>
+                        A propos
+                    </Link>
                 </li>
             </ul>
         </div>

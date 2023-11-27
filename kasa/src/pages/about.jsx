1fa5579f2c763c801a4d8c192792daeffc575405
Claudie.Michalon <michalon.claudie@outlook.fr>
2023-11-27 10:42:28 +0100

    "finishing media querries about pages"

--- kasa/src/pages/about.jsx
+++ kasa/src/pages/about.jsx
@@ -4,6 +4,7 @@ import '../style/pages/about.scss'
 import Header from '../component/Header'
 import Banner from '../component/Banner'
 import Collapse from '../component/Collapse'
+import Footer from '../component/Footer'
 
 export default function About()
 {
@@ -30,6 +31,7 @@ export default function About()
                 content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. 
                 Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."/>
             </section>
+            <Footer/>
         </div>
     )
 }
